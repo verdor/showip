13f8210688873a79167627d8532394b9e1037184
el verdor <el.verdor@gmail.com>
2014-04-02 21:04:01 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Showip::Application.config.secret_key_base = '3922a0901909a5530dd7aa620ac0fd0a58e269885653dc0047795b725a6026543c01148ab84ea8e6650f067c43ced71e650ac0a4b1ec311151a43df7a2f4736b'
