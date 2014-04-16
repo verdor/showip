a1f93980167d204672fcdcc3869bd16986c03b69
el verdor <el.verdor@gmail.com>
2014-04-16 23:14:30 +0200

    check_revision

--- config/deploy.rb
+++ config/deploy.rb
@@ -89,4 +89,14 @@ namespace :deploy do
       /etc/init.d/showip restart
     CMD
   end
+
+  desc "Make sure local git is in sync with remote."
+  task :check_revision, roles: :web do
+    unless `git rev-parse HEAD` == `git rev-parse origin/master`
+      puts "WARNING: HEAD is not the same as origin/master"
+      puts "Run `git push` to sync changes."
+      exit
+    end
+  end
+  before "deploy", "deploy:check_revision"
 end
